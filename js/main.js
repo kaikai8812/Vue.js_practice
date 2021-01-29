d1ab52724f91a27a0baed8c93b3fec7515678a73
kaikai8812 <sauna.kaikai@gmail.com>
2021-01-29 13:57:39 +0900

    todo list 削除昨日まで

---
+++ js/main.js
@@ -0,0 +1,34 @@
+(function(){
+  'use strict';
+
+// var vm =new Vue({
+//   el: '#app',
+//   data: {
+//     name: 'taguchi'
+//   }
+// });
+var vm = new Vue({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: [
+      'task1',
+      'task2',
+      'task3'
+    ]
+  },
+  methods: {
+    addItem: function(e){
+      e.preventDefault();
+      this.todos.push(this.newItem);   //newItemを、todos(配列)にpush(追加)する。
+      this.newItem = '';
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index,1)  //ここでindexを使ってどれを消すか選別する。
+    }
+  }
+});
+
+
+
+})();
